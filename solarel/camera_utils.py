6712eba0f0d123f2373492568db17058a1120297
Charlie Howcroft <ch61@utas.edu.au>
2023-03-07 17:30:32 +1100

    cell splitter 3

--- solarel/camera_utils.py
+++ solarel/camera_utils.py
@@ -1,19 +1,21 @@
 import cv2
 import numpy as np
 import toml
+import matplotlib.pyplot as plt
 
 
 def fix_barrel_distortion(image: np.ndarray, config_file_path: str) -> np.ndarray:
-    
-    with open('configs/camera_config.toml', 'r') as f:
+    with open(config_file_path, 'r') as f:
         config = toml.load(f)
 
-    newcameramtx = config['camera']['camera_matrix']
-    mtx = config['camera']['mtx']
-    dist = config['camera']['dist']
+    newcameramtx = np.array(config['camera']['camera_matrix'], np.float32)
+    mtx = np.array(config['camera']['mtx'], np.float32)
+    dist = np.array(config['camera']['dist'], np.float32)
 
-    h,w = image.shape[:2]
-    newcameramtx, roi = cv2.getOptimalNewCameraMatrix(mtx, dist, (w,h), 1, (w,h))
-    mapx,mapy=cv2.initUndistortRectifyMap(mtx,dist,None,newcameramtx,(w,h),5)
-    dst = cv2.remap(image, mapx,mapy,cv2.INTER_LINEAR)
+    h, w = image.shape[:2]
+    newcameramtx, roi = cv2.getOptimalNewCameraMatrix(
+        mtx, dist, (w, h), 1, (w, h))
+    mapx, mapy = cv2.initUndistortRectifyMap(
+        mtx, dist, None, newcameramtx, (w, h), 5)
+    dst = cv2.remap(image, mapx, mapy, cv2.INTER_LINEAR)
     return dst
